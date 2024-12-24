1f04457a93649a6438c20a116c8b8a0e2cf4237e
siddharthjkamra <sidhrthkmr@gmail.com>
2024-12-24 12:20:56 +0530

    update

---
+++ assets/import.js
@@ -0,0 +1,32 @@
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    file = elmnt.getAttribute("katana");
+    if (file) {
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {
+            elmnt.innerHTML = this.responseText;
+          }
+          if (this.status == 404) {
+            elmnt.innerHTML = "Page not found.";
+          }
+          elmnt.removeAttribute("katana");
+          includeHTML();
+
+          window.scrollTo(0, 0);
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      return;
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  includeHTML();
+});
